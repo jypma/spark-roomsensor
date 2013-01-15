edfeac88e1d39a4153dac896873c678d4e121fd8
Jan Ypma <github@jan.ypmania.nl>
2013-01-15 11:27:36 +0100

    power save now working. remember the cap!

--- main.cpp
+++ main.cpp
@@ -1,26 +1,49 @@
-#include <Arduino.h>
-
-#include <JeeLib.h>
 #include <avr/eeprom.h>
 #include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/sleep.h>
+
+#include <Arduino.h>
+#include <JeeLib.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define PIN_LED 9
-#define PIN_TEMP 5
-#define TEMP_INTERVAL 10000
-//#define DEBUG
+ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 
-unsigned char payload[] = "R2     ";
+#define PIN_LED 9
+#define PIN_TEMP 8
+#define TEMP_INTERVAL 60000
+#define DEBUG
+
+const long InternalReferenceVoltage = 1095L;  // Change this to the reading from your internal voltage reference
+
+unsigned char payload[] = "R3       ";
 //                           ^^------- recipient, always spaces for broadcast
 //                             ^------ 1 = temperature
 //                              ^^---- temperature, signed 16-bit int
+//                                ^^-- battery, centivolts
 
 OneWire oneWire(PIN_TEMP);
 DallasTemperature sensors(&oneWire);
 DeviceAddress tempAddress;
 
-ISR(WDT_vect) { Sleepy::watchdogEvent(); }
+void startADC() {
+	// REFS1 REFS0          --> 0 0 AREF, Internal Vref turned off
+	// MUX3 MUX2 MUX1 MUX0  --> 1110 1.1V (VBG)
+    ADMUX = (0<<REFS1) | (0<<REFS0) | (0<<ADLAR) | (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (0<<MUX0);
+
+    // Start a conversion
+    ADCSRA |= _BV( ADSC );
+}
+
+int endADC() {
+    // Wait for it to complete
+    while( ( (ADCSRA & (1<<ADSC)) != 0 ) );
+
+    // Scale the value
+    return (((InternalReferenceVoltage * 1023L) / ADC) + 5L) / 10L;
+}
 
 void printAddress(DeviceAddress deviceAddress)
 {
@@ -57,6 +80,19 @@ void setup () {
     pinMode(PIN_LED, OUTPUT);
 }
 
+int getSupplyVoltage() {
+    startADC();
+    endADC();
+    startADC();
+    endADC();
+    int voltage = 0;
+    for (byte b = 0; b < 16; b++) {
+        startADC();
+        voltage += endADC();
+    }
+    return voltage >> 4;
+}
+
 void sendTempPacket() {
     digitalWrite(PIN_LED, HIGH);
 
@@ -68,9 +104,24 @@ void sendTempPacket() {
     Serial.print("*");
     Serial.flush();
 #endif
+    digitalWrite(PIN_LED, LOW);
     Sleepy::loseSomeTime(1000);
+    digitalWrite(PIN_LED, HIGH);
+
 #ifdef DEBUG
     Serial.print("*");
+    Serial.flush();
+#endif
+    int voltage = getSupplyVoltage();
+#ifdef DEBUG
+    Serial.print(voltage);
+    Serial.flush();
+#endif
+    *((int*)(payload + 7)) = voltage;
+
+#ifdef DEBUG
+    Serial.print("*");
+    Serial.flush();
 #endif
 
 	rf12_sleep(RF12_WAKEUP);
@@ -108,7 +159,9 @@ void sendTempPacket() {
 
 void loop () {
 	sendTempPacket();
-	Sleepy::loseSomeTime(TEMP_INTERVAL);
+	ADCSRA &= ~(1<<ADEN);
+    Sleepy::loseSomeTime(TEMP_INTERVAL);
+    ADCSRA |=  (1<<ADEN);
 }
 
 int main(void) {
