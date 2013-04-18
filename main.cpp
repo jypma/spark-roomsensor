2e94142f86cf998a3f3f396a9dafff3261258d60
Jan Ypma <github@jan.ypmania.nl>
2013-04-18 19:20:15 +0200

    profiles per room

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
+/*                 JeeNode / JeeNode USB / JeeSMD
+     -------|-----------------------|----|-----------------------|----
+    |       |D3  A1 [Port2]  D5     |    |D3  A0 [port1]  D4     |    |
+    |-------|IRQ AIO +3V GND DIO PWR|    |IRQ AIO +3V GND DIO PWR|    |
+    | D1|TXD|                                           ---- ----     |
+    | A5|SCL|                                       D12|MISO|+3v |    |
+    | A4|SDA|   Atmel Atmega 328                    D13|SCK |MOSI|D11 |
+    |   |PWR|   JeeNode / JeeNode USB / JeeSMD         |RST |GND |    |
+    |   |GND|                                       D8 |BO  |B1  |D9  |
+    | D0|RXD|                                           ---- ----     |
+    |-------|PWR DIO GND +3V AIO IRQ|    |PWR DIO GND +3V AIO IRQ|    |
+    |       |    D6 [Port3]  A2  D3 |    |    D7 [Port4]  A3  D3 |    |
+     -------|-----------------------|----|-----------------------|----
+*/
+
 #include <avr/eeprom.h>
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
@@ -6,27 +21,58 @@
 
 #include <Arduino.h>
 #include <JeeLib.h>
+
+#include "stue.h"
+
+#define DEBUG
+
+#ifdef DS18_PIN
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#endif
+
+#ifdef DHT_PIN
+#include <DHT.h>
+#endif
 
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 
-#define PIN_LED 9
-#define PIN_TEMP 8
-#define TEMP_INTERVAL 60000
-#define DEBUG
-
 const long InternalReferenceVoltage = 1095L;  // Change this to the reading from your internal voltage reference
 
-unsigned char payload[] = "R3       ";
+#ifdef DHT_PIN
+unsigned char payload[] = "R1         ";
+//                           ^^--------- recipient, always spaces for broadcast
+//                             ^-------- 1 = temperature
+//                              ^^------ temperature, 1/100 deg celcius, signed 16-bit int
+//                                ^^---- battery, centivolts
+//                                  ^^-- humidity, %
+#else
+unsigned char payload[] = "R1       ";
 //                           ^^------- recipient, always spaces for broadcast
 //                             ^------ 1 = temperature
 //                              ^^---- temperature, signed 16-bit int
 //                                ^^-- battery, centivolts
+#endif
 
-OneWire oneWire(PIN_TEMP);
+#ifdef DS18_PIN
+OneWire oneWire(DS18_PIN);
 DallasTemperature sensors(&oneWire);
 DeviceAddress tempAddress;
+
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    // zero pad the address if necessary
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+#endif
+
+#ifdef DHT_PIN
+DHT dht(DHT_PIN, DHT_TYPE);
+#endif
 
 void startADC() {
 	// REFS1 REFS0          --> 0 0 AREF, Internal Vref turned off
@@ -45,21 +91,13 @@ int endADC() {
     return (((InternalReferenceVoltage * 1023L) / ADC) + 5L) / 10L;
 }
 
-void printAddress(DeviceAddress deviceAddress)
-{
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    // zero pad the address if necessary
-    if (deviceAddress[i] < 16) Serial.print("0");
-    Serial.print(deviceAddress[i], HEX);
-  }
-}
-
 void setup () {
     Serial.begin(57600);
     Serial.print("\n[RoomSensor]");
     rf12_initialize(3, RF12_868MHZ, 5);
+    payload[1] = ROOM_SENSOR_ID;
 
+#ifdef DS18_PIN
     sensors.begin();
     Serial.print("Found ");
     Serial.print(sensors.getDeviceCount());
@@ -76,8 +114,14 @@ void setup () {
 
     sensors.setResolution(tempAddress, 12);
     sensors.setWaitForConversion(false);
+#endif
 
-    pinMode(PIN_LED, OUTPUT);
+#ifdef DHT_PIN
+    dht.begin();
+    Serial.println("DHT initialized");
+#endif
+
+    pinMode(LED_PIN, OUTPUT);
 }
 
 int getSupplyVoltage() {
@@ -94,24 +138,27 @@ int getSupplyVoltage() {
 }
 
 void sendTempPacket() {
-    digitalWrite(PIN_LED, HIGH);
+    digitalWrite(LED_PIN, HIGH);
 
 #ifdef DEBUG
     Serial.print("[");
 #endif
+
+#ifdef DS18_PIN
     sensors.requestTemperatures();
 #ifdef DEBUG
     Serial.print("*");
     Serial.flush();
 #endif
-    digitalWrite(PIN_LED, LOW);
+    digitalWrite(LED_PIN, LOW);
     Sleepy::loseSomeTime(1000);
-    digitalWrite(PIN_LED, HIGH);
-
+    digitalWrite(LED_PIN, HIGH);
 #ifdef DEBUG
     Serial.print("*");
     Serial.flush();
 #endif
+#endif
+
     int voltage = getSupplyVoltage();
 #ifdef DEBUG
     Serial.print(voltage);
@@ -126,7 +173,12 @@ void sendTempPacket() {
 
 	rf12_sleep(RF12_WAKEUP);
 
+#ifdef DS18_PIN
     float tempC = sensors.getTempC(tempAddress);
+#elif DHT_PIN
+    float tempC = dht.readTemperature();
+#endif
+
 #ifdef DEBUG
     Serial.print(tempC);
 #endif
@@ -138,10 +190,24 @@ void sendTempPacket() {
 #ifdef DEBUG
     Serial.print("*");
 #endif
+
+#ifdef DHT_PIN
+    float hum = dht.readHumidity();
+#ifdef DEBUG
+    Serial.print(hum);
+#endif
+    int h = (int) (hum * 100);
+    *((int*)(payload + 9)) = (int) h;
+#ifdef DEBUG
+    Serial.print("*");
+#endif
+#endif
+
     // send our packet, once possible
     while (!rf12_canSend()) rf12_recvDone();
 #ifdef DEBUG
     Serial.print("*");
+    Serial.flush();
 #endif
 
     rf12_sendStart(0, payload, sizeof payload);
@@ -154,7 +220,7 @@ void sendTempPacket() {
     Serial.println("]");
 	Serial.flush();
 #endif
-    digitalWrite(PIN_LED, LOW);
+    digitalWrite(LED_PIN, LOW);
 }
 
 void loop () {
---
+++ test.h
@@ -0,0 +1,6 @@
+#define TEMP_INTERVAL 2000
+#define DS18_PIN 8      // comment out if room sensor doesn't have DS18 sensor
+#define DHT_PIN 5      // comment out if room sensor doesn't have DHT sensor
+#define DHT_TYPE DHT11 // DHT11, DHT22, DHT21
+#define LED_PIN 9
+#define ROOM_SENSOR_ID '-'
